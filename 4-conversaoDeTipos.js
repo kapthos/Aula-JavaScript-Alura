439bd147d0d517a489b0410b50695515af5edf32
kapthos <81396249+kapthos@users.noreply.github.com>
2021-10-11 03:43:06 -0300

    5 primeiras aulas

---
+++ 4-conversaoDeTipos.js
@@ -0,0 +1,7 @@
+console.log("Conversão De Tipos");
+
+console.log("ano" + 2021); //concateção de texto + numero
+console.log("2" + "2"); //concateção de texto
+console.log(parseInt("2") + parseInt("2")); //forçar texto para numero com parse
+console.log("10" / "2"); //mesmo sendo texto, ele consegue dividir por ser a unica coisa lógica a ser feita
+console.log("Juan" / 2); //mostra NaN por não ser número
---
+++ 5-atribuicaoVariaveis.js
@@ -0,0 +1,6 @@
+console.log("Trabalhando com atribuição de variaveis");
+
+const primeiroNome = "Juan";
+const sobrenome = "Torterolo";
+
+console.log(`Meu nome é ${primeiroNome} ${sobrenome}`); //crase para colocar variaveis dentro de strings
