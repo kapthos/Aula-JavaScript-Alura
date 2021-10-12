9149f8d87e4ab91a0471a769002836e88547d1b1
kapthos <81396249+kapthos@users.noreply.github.com>
2021-10-11 23:38:01 -0300

    Create 6-listas.js

---
+++ 6-listas.js
@@ -0,0 +1,11 @@
+console.log(`Trabalhando com listas`);
+
+const listaDeDestinos = new Array (
+    'São Paulo',
+    'Rio de Janeiro',
+    'Salvador'
+);
+
+listaDeDestinos.push('Curitiba'); //adiciona um item na lista
+
+console.log(listaDeDestinos);
